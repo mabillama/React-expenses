9e0df7aff593b70046cb7f731faf81a53d8ba9a0
Marc Abillama <mabillama@gmail.com>
2022-01-07 16:09:14 +0100

    Props refactor

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,19 +1,17 @@
 // import ReactDOM from "react-dom";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2021, 2, 28);
-  // const expenseTitle = "Car Insurance";
-  // const expenseAmount = 294.67;
-
   return (
-    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
