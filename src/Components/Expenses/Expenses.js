3c7bfab200838f547a11eaa9ce710134ffe1b20b
Marc Abillama <mabillama@gmail.com>
2022-01-13 11:44:05 +0100

    Updated filter logic. Removed button logic for updating title of expense

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -22,14 +22,18 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredExpenses.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
